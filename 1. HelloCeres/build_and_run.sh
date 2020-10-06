159c3c70d9043085e9f6a5f0b66e13effa767111
Giseop Kim <kkissbee@kaist.ac.kr>
2020-10-06 16:02:41 +0900

    Add files via upload

---
+++ 1. HelloCeres/build_and_run.sh
@@ -0,0 +1,8 @@
+# how to use: 
+# $ sh build_and_run.sh
+
+mkdir build 
+cd build 
+cmake ..
+make 
+./helloworld
---
+++ 1. HelloCeres/main.cpp
@@ -0,0 +1,52 @@
+#include "ceres/ceres.h"
+#include "glog/logging.h"
+
+using ceres::AutoDiffCostFunction;
+using ceres::CostFunction;
+using ceres::Problem;
+using ceres::Solve;
+using ceres::Solver;
+
+// A templated cost functor that implements the residual r = 10 -
+// x. The method operator() is templated so that we can then use an
+// automatic differentiation wrapper around it to generate its
+// derivatives.
+struct MyCostFunc {
+  template <typename T>
+  bool operator()(const T* const x, T* residual) const {
+    residual[0] = 10.0 - x[0];
+    return true;
+  }
+};
+
+
+int main(int argc, char** argv) {
+
+    google::InitGoogleLogging(argv[0]);
+
+    // The variable to solve for with its initial value.
+    double initial_x = 5.0;
+    double x = initial_x;
+
+    // Build the problem.
+    Problem problem;
+
+    // Set up the only cost function (also known as residual). This uses
+    // auto-differentiation to obtain the derivative (jacobian).
+    CostFunction* cost_function =
+        new AutoDiffCostFunction<MyCostFunc, 1, 1>(new MyCostFunc);
+    problem.AddResidualBlock(cost_function, NULL, &x);
+
+    // Run the solver!
+    Solver::Options options;
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
+    Solver::Summary summary;
+    Solve(options, &problem, &summary);
+
+    std::cout << summary.BriefReport() << "\n";
+    std::cout << "x : " << initial_x
+            << " -> " << x << "\n";
+
+    return 0;
+}
